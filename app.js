0f11167993531f7f2f20ddd8e84ef87577ad8a1c
Lee SeJin <lsj8706@naver.com>
2021-03-22 23:09:29 +0900

    templates update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -14,12 +15,14 @@ const app = express();
 
 //app.use(betweenHome);
 //middleware가 모든 get마다 실행되게함
+app.use(helmet());
+app.set('view engine',"pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan('dev'));
 
+app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);

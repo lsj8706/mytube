8ecef44be2b8008d722d377ab6e451122864556e
Lee SeJin <lsj8706@naver.com>
2021-03-25 21:36:42 +0900

    pug

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # MyTube
 
 Mytube with Vanilla and NodeJS!!
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
+   
---
+++ db.js
@@ -0,0 +1,51 @@
+export const videos = [
+    {
+        id:324393,
+        title: 'Video awesom',
+        description: 'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name:"Lee",
+            email:"lee@se.com"
+        }
+    },
+    {
+        id:12345,
+        title: 'Video super',
+        description: 'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name:"Lee",
+            email:"lee@se.com"
+        }
+    },
+    {
+        id:13414,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name:"Lee",
+            email:"lee@se.com"
+        }
+    },
+    {
+        id:283838,
+        title: 'Video good',
+        description: 'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name:"Lee",
+            email:"lee@se.com"
+        }
+    }
+
+]

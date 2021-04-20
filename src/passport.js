97d63a98f36d694602a8084cdd47c571b6f5cd9f
Lee SeJin <lsj8706@khu.ac.kr>
2021-04-20 23:06:04 +0900

    scripts

--- src/passport.js
+++ src/passport.js
@@ -20,7 +20,7 @@ passport.use(new GithubStrategy(
 passport.use(new KakaoStrategy(
     {
         clientID : process.env.KAKAO_ID,
-        clientSecret: process.env.KAKAO_SECRET, // clientSecret을 사용하지 않는다면 넘기지 말거나 빈 스트링을 넘길 것
+        clientSecret: process.env.KAKAO_SECRET,
         callbackURL : `http://localhost:4000/${routes.kakaotalkCallback}`
     },
     kakaotalkLoginCallback
